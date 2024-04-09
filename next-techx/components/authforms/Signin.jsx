bebff19dd6145fe34d5606f0484d47991789ec44
1lexaa <nice140@icloud.com>
2024-04-09 21:16:01 +0300

    gg

--- next-techx/components/authforms/Signin.jsx
+++ next-techx/components/authforms/Signin.jsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { useRouter } from "next/navigation";
+import { set } from "mongoose";
 
 const Signin = () => {
   const [email, setEmail] = useState("");
@@ -101,15 +102,20 @@ const Signin = () => {
               console.log("Sessia is not complite");
             }
           } catch (error) {
+            setError("Error during signing the token.");
             setShowAlert(true);
             console.error("Error during signing the token:", error);
           }
         } else {
-          console.log("SIGN IN: false");
+          setShowAlert(true);
+          setError("Password is incorrect");
+          console.error("SIGN IN: false", error);
           return;
         }
       } else {
-        console.log("User not found");
+        setShowAlert(true);
+        setError("User not found");
+        console.error("User not found");
         return;
       }
     } catch (error) {
